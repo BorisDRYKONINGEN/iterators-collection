a064c489dd6a10d6bde8240b924824589f04ad28
BorisD <boris.d@orange.fr>
2019-10-31 18:35:05 +0100

    type written but not tested yet because it would force me to make additionnal changes I want to do later

--- src/core/mod.rs
+++ src/core/mod.rs
@@ -14,4 +14,49 @@ pub trait ResettableIterator: Iterator {
         new.reset();
         new
     }
+
+    /// Creates a ResettableMap from the current iterator
+    fn resettable_map<F, R>(self, callback: F) -> self::ResettableMap<Self, F>
+    where
+        F: FnMut(Self::Item) -> R,
+        Self: Sized,
+    {
+        ResettableMap {
+            iterator: self,
+            callback,
+        }
+    }
+}
+
+/// A resettable version of `std::iter::Map`. A simple trait implementation is not suitable because it requires to get access to private elements of `std::iter::Map` like the iterator stored
+/// 
+/// You can use it like you would use `std::iter::Map` but it implements the `ResettableIterator` trait too
+pub struct ResettableMap<I, F> {
+    iterator: I,
+    callback: F,
+}
+
+impl<I, F, R> Iterator for ResettableMap<I, F>
+where
+    I: Iterator,
+    F: FnMut(I::Item) -> R,
+{
+    type Item = R;
+
+    fn next(&mut self) -> Option<R> {
+        let item = self.iterator.next()?;
+        let result = (self.callback)(item);
+
+        Some(result)
+    }
+}
+
+impl<I, F, R> self::ResettableIterator for ResettableMap<I, F>
+where
+    I: self::ResettableIterator,
+    F: FnMut(I::Item) -> R,
+{
+    fn reset(&mut self) {
+        self.iterator.reset();
+    }
 }
